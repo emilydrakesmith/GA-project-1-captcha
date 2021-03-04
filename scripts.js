9445f6af20eeaed06ccc1f3b1e20957bb646b259
mhsmith321 <marty.smith01@yahoo.com>
2021-03-04 14:44:16 -0500

    format api results into objects

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+/******* HTML GENERATION FUNCTIONS *******/
+
 const body = document.getElementById('body');
 
 function renderInitialContainers() {
@@ -6,34 +8,60 @@ function renderInitialContainers() {
                                            <footer id='footer'></footer>`);
 }
 
+/******* EVENT LISTENER FUNCTIONS *******/
+
 /******* API CALL FUNCTIONS *******/
 
 function getDogImage() {
-    fetch('https://dog.ceo/api/breeds/image/random')
+    return fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => formatDogImage(data))
         .catch(err => console.log(err));
 }
 
 function getCatImage() {
-    fetch('https://aws.random.cat/meow')
+    return fetch('https://aws.random.cat/meow')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => formatCatImage(data))
         .catch(err => console.log(err));
 }
 
 function getPizzaImage() {
-    fetch('https://foodish-api.herokuapp.com/api/images/pizza')
+    return fetch('https://foodish-api.herokuapp.com/api/images/pizza')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => formatPizzaImage(data))
         .catch(err => console.log(err));
+}
+
+/******* IMAGE-OBJECT FORMATTING FUNCTIONS *******/
+
+const formatDogImage = dogData => ({'type': 'dog', 'imageURL': dogData.message});
+
+const formatCatImage = catData => ({'type': 'cat', 'imageURL': catData.file});
+
+const formatPizzaImage = pizzaData => ({'type': 'pizza', 'imageURL': pizzaData.image});
+
+async function createImageSet() {
+    let imageSet= [];
+    for (let i=0; i<2; i++) {
+        imageSet.push(await getDogImage());
+        imageSet.push(await getCatImage());
+        imageSet.push(await getPizzaImage());
+    }
+    for (let i=imageSet.length-1; i>0; i--) {
+        const j = Math.floor(Math.random() * (i+1));
+        [imageSet[i], imageSet[j]] = [imageSet[j], imageSet[i]];
+    }
+    console.log(imageSet)
+    imageSet.forEach(item => console.log(item));
+    return imageSet;
 }
 
 /******* INITIALIZATION FUNCTION *******/
 
 function initialize() {
     renderInitialContainers();
-    getPizzaImage();
+    createImageSet();
 }
 
 initialize();
